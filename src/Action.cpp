d4369f0458c2e82d3184fb2cc96c6dd7af383dbc
spl211 <ishai.picus@gmail.com>
2021-11-21 13:24:16 +0200

    -customer,workout finish-

--- src/Action.cpp
+++ src/Action.cpp
@@ -140,8 +140,13 @@ void PrintTrainerStatus::act(Studio &studio) {
         std::cout <<"Orders:\n";
         std::vector<OrderPair> orderList = trainer->getOrders();
         for (int i = 0; i < orderList.size(); ++i) {
-            std::cout <<orderList[i].second.getName()<<" " <<std::to_string(orderList[i].second.getPrice())<< " "<< std::to_string(orderList[i].first)
+            std::cout <<orderList[i].second.getName()<<" " <<std::to_string(orderList[i].second.getPrice())<< " "<< std::to_string(orderList[i].first)<<"\n";
         }
+        int sum =0;
+        for(int i =0; i<orderList.size(); i++){
+            sum = sum + orderList[i].second.getPrice();
+        }
+        std::cout <<"Current Trainer’s Salary: " << std::to_string(trainer->getSalary() + sum)<<"\n";
     }
 
 }
