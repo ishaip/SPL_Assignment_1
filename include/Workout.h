0f6b31223f90098f314760aec54d37f508288d2c
picus@wincs.cs.bgu.ac.il <picus>
2021-11-26 09:58:15 +0200

    -meged with itai-

--- include/Workout.h
+++ include/Workout.h
@@ -20,6 +20,7 @@ public:
     std::string typeToString();
     virtual ~Workout(); //destructor
     Workout(const Workout &w);
+    virtual Workout *clone();
 private:
 	const int id;
     const std::string name;
